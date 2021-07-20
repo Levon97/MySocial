aad79d0f251c2e22f70e243895e4a8aef2cadba6
Levon97 <levon.samsonyan@gmail.com>
2021-07-21 00:33:36 +0400

    setting up server

---
+++ configs.js
@@ -0,0 +1,16 @@
+const db = {
+    url: 'mongodb+srv://levon:dbUserFeanor@projectx.0vbdk.mongodb.net/MySocial?retryWrites=true&w=majority',
+    options: {useNewUrlParser: true, useUnifiedTopology: true}
+  }; 
+
+
+const env ={
+    PORT: process.env.PORT || 3000
+  };
+
+
+ module.exports = { 
+     db,
+     env,
+     
+ }
---
+++ src/server.js
@@ -0,0 +1,17 @@
+const configs = require('../configs');
+
+const express = require('express');
+const  mongoose = require('mongoose')
+const app = express();
+
+(async () => {
+    try {
+        await mongoose.connect(configs.db.url, configs.db.options);
+        console.log('Connection to DB Successful');
+        app.listen(configs.env.PORT, () => {
+            console.log(`Server is running ${configs.env.PORT}`);
+          });
+    } catch (err) {
+        console.log('Connection to DB Failed',err);
+    }
+})();
